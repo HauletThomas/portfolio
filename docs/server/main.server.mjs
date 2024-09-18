9f14f240f9def7f803d9cad2fb78c23d65501a96
HauletThomas <thomashaulet@gmail.com>
2024-09-18 12:54:50 +0200

    correction

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-QYILGNZL.mjs";import"./chunk-SBZZ5CEY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-RVQMVWV5.mjs";import"./chunk-KGYTEIMS.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/_services/projects.service.ts
+++ src/app/_services/projects.service.ts
@@ -9,40 +9,40 @@ export class ProjectsService {
 
   projects: Project[] = [
     {id: 0,
-    name: 'Project PHP for a ASBL',
-    summary: '2nd year project',
-    description: 'Project for a non-profit organization, making appointments, several roles, recording payments list of activities, sessions, participation',
-    projectLink: "",
-    pictures: ['./assets/angPica4.PNG'],
-    tags: [Tag.PHP, Tag.CSS, Tag.HTML, Tag.MVC, Tag.JS]
-  },{id: 1,
-    name: 'Intership Project',
-    summary: 'Dashboard using d3.js',
-    description: 'Visualization and supervision of a data management and processing system.',
-    projectLink: "",
-    pictures: ['./assets/angPica4.PNG'],
-    tags: [Tag.ANGULAR, Tag.REST, Tag.JSON, Tag.HTML, Tag.CSS]
-  },{id: 2,
     name: 'First PHP project',
     summary: 'WWE showcase site',
     description: 'First site that i make on the history of the WWE, show the PLEs, show the champions and be able to change them by being admin, a forum to exchange, and registration',
     projectLink: "https://webacademy.be/projets/2020/wwe_fan_belgium/index.html",
     pictures: ['./assets/wwe.png'],
     tags: [Tag.HTML, Tag.PHP, Tag.CSS]
-  },{id: 3,
+  },{id: 1,
     name: 'Java App',
     summary: '3nd year project',
     description: 'Site for helha students to be able to exchange documents and discuss different subjects. An admin party for the moderation of all the site',
     projectLink: "",
-    pictures: ['./assets/angPica4.PNG'],
+    pictures: ['./assets/helhahelp.png'],
     tags: [Tag.JAVA, Tag.ANGULAR, Tag.CSS]
-  },{id: 4,
+  },{id: 2,
     name: 'Angular Front-end',
     summary: '3nd year project',
     description: 'Front end of an application to post photos and add likes, you can see your profile, the last 10 posts that users have posted, a comment/description of the photo',
     projectLink: "",
     pictures: ['./assets/angPica4.PNG'],
     tags: [Tag.ANGULAR, Tag.CSS]
+  },{id: 3,
+    name: 'Project PHP for a ASBL',
+    summary: '2nd year project',
+    description: 'Project for a non-profit organization, making appointments, several roles, recording payments list of activities, sessions, participation',
+    projectLink: "",
+    pictures: ['./assets/angPica4.PNG'],
+    tags: [Tag.PHP, Tag.CSS, Tag.HTML, Tag.MVC, Tag.JS]
+  },{id: 4,
+    name: 'Intership Project',
+    summary: 'Dashboard using d3.js',
+    description: 'Visualization and supervision of a data management and processing system.',
+    projectLink: "",
+    pictures: ['./assets/angPica4.PNG'],
+    tags: [Tag.ANGULAR, Tag.REST, Tag.JSON, Tag.HTML, Tag.CSS]
   },
   ]
 
